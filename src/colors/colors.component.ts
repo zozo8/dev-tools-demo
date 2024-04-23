62ac40a555d9136a631d4f5a73bfff26e783d6b2
zozo8 <pawel@zochowski.eu>
2024-04-23 21:32:34 +0200

    initial

---
+++ src/colors/colors.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'colors',
+    standalone: true,
+    templateUrl: `colors.component.html`,
+  })
+  export class ColorsComponent {
+  }
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,23 @@
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { Route, RouterModule, provideRouter } from '@angular/router';
 import 'zone.js';
+import { ColorsComponent } from './colors/colors.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
-  `,
+  templateUrl: `main.html`,
+  imports: [RouterModule]
 })
 export class App {
   name = 'Angular';
 }
 
-bootstrapApplication(App);
+export const ROUTES: Route[] = [
+  {path: 'colors', component: ColorsComponent},
+];
+
+bootstrapApplication(App, {
+  providers: [provideRouter(ROUTES)],
+});
